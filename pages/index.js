8ff9e6bfa403c7ea14ba03200403e3c6ede106da
Gabriel Luis <gabriel@lilrobots.com>
2022-10-16 20:23:45 -0500

    Add page components and dynamic routing

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,17 @@
 import Head from 'next/head'
 import Container from '../components/container'
+import MorePages from '../components/more-pages'
 import MoreStories from '../components/more-stories'
 import HeroPost from '../components/hero-post'
 import Intro from '../components/intro'
 import Layout from '../components/layout'
 import { CMS_NAME } from '../lib/constants'
-import { indexQuery } from '../lib/queries'
+import { postIndexQuery, pageIndexQuery } from '../lib/queries'
 import { usePreviewSubscription } from '../lib/sanity'
 import { getClient, overlayDrafts } from '../lib/sanity.server'
 
 export default function Index({ allPosts: initialAllPosts, preview }) {
-  const { data: allPosts } = usePreviewSubscription(indexQuery, {
+  const { data: allPosts } = usePreviewSubscription(postIndexQuery, {
     initialData: initialAllPosts,
     enabled: preview,
   })
@@ -40,8 +41,28 @@ export default function Index({ allPosts: initialAllPosts, preview }) {
   )
 }
 
+// export default function Index({ allPages: initialAllPages, preview }) {
+//   const { data: allPages } = usePreviewSubscription(pageIndexQuery, {
+//     initialData: initialAllPages,
+//     enabled: preview,
+//   })
+//   return (
+//     <>
+//       <Layout preview={preview}>
+//         <Head>
+//           <title>Next.js Blog Example with {CMS_NAME}</title>
+//         </Head>
+//         <Container>
+//           <Intro />
+//           {morePages.length > 0 && <MoreStories posts={morePages} />}
+//         </Container>
+//       </Layout>
+//     </>
+//   )
+// }
+
 export async function getStaticProps({ preview = false }) {
-  const allPosts = overlayDrafts(await getClient(preview).fetch(indexQuery))
+  const allPosts = overlayDrafts(await getClient(preview).fetch(postIndexQuery))
   return {
     props: { allPosts, preview },
     // If webhooks isn't setup then attempt to re-generate in 1 minute intervals
