70c0e8c8e75f0bd1505983b073f58d06629b576d
Gabriel Luis <gabriel@lilrobots.com>
2022-10-26 17:02:23 -0500

    Create about.js

---
+++ pages/about.js
@@ -0,0 +1,167 @@
+import React from 'react'
+import Head from 'next/head'
+import Layout from '../components/layout'
+
+export default function About() {
+  return (
+    <Layout>
+      <Head>
+        <title>
+          About Baxter IP, Patent & Trade Mark Attorneys | Baxter IP
+        </title>
+        <meta
+          name="description"
+          content="Baxter IP is a leading, independent, mid-tier patent and trade mark attorney firm with offices in Sydney and Melbourne, Australia."
+        />
+        <link
+          rel="canonical"
+          href="/about"
+        />
+      </Head>
+      <h1>About Baxter IP, Patent & Trade Mark Attorneys</h1>
+      <h2>A patent attorney firm for innovators</h2>
+      <p>
+        Innovative technologies, new product development and bright ideas that
+        are both unique and highly commercial are a rare and valuable commodity.
+        If you or a team member have invented or developed an impactful
+        innovation, you need a{' '}
+        <a href="https://www.baxterip.com.au/patent-attorney">
+          patent attorney
+        </a>{' '}
+        firm that:
+      </p>
+      <ol>
+        <li>
+          has a deep understanding of your area of technology, that can get to
+          the core of your idea quickly and provide careful patent &amp; trade
+          mark attorney counsel;
+        </li>
+        <li>
+          that is experienced in the patent end game – negotiation, licensing
+          and enforcement; and
+        </li>
+        <li>
+          understands and can integrate your IP with the entire innovation
+          process from protection to commercialisation, capital raising and
+          development.
+        </li>
+      </ol>
+      <p>You need an IP firm that has walked the walk.</p>
+      <h2>A trade mark attorney firm for distinctive brands:</h2>
+      <p>
+        Often equally or even more valuable than unique ideas, are distinctive
+        brands that are recognised by their target markets. If your business has
+        created or is using a brand you do not want others to use, you need a{' '}
+        <a href="https://www.baxterip.com.au/trade-mark-attorney">
+          trade mark attorney
+        </a>{' '}
+        firm that:
+      </p>
+      <ol>
+        <li>
+          Can help you choose the right brand or{' '}
+          <a href="https://www.baxterip.com.au/types-of-trade-marks">
+            type of trade marks
+          </a>{' '}
+          to file based on the learning arising from the team’s voluminous track
+          record of trade mark experience
+        </li>
+        <li>Is highly responsive and transparent on pricing</li>
+      </ol>
+      <h2>Baxter IP identifies, protects and grows your IP</h2>
+      <p>
+        With offices in <a href="https://www.baxterip.com.au/sydney">Sydney</a>{' '}
+        and <a href="https://www.baxterip.com.au/melbourne">Melbourne</a>, we
+        are a mid-sized firm that offers the combined resources of a close-knit
+        network of highly experienced{' '}
+        <a href="https://www.baxterip.com.au/ip-attorney">
+          patent &amp; trade mark attorneys
+        </a>
+        , specialists in{' '}
+        <a href="https://www.baxterip.com.au/specialisations/software-patents">
+          software
+        </a>
+        ,{' '}
+        <a href="https://www.baxterip.com.au/specialisations/mechanical-patents">
+          engineering
+        </a>{' '}
+        and{' '}
+        <a href="https://www.baxterip.com.au/specialisations/pharmaceutical-patents">
+          pharmaceuticals
+        </a>{' '}
+        IP and trade marks, who are poised to protect what’s yours.
+      </p>
+      <p>
+        Our 2000+ strong client base ranges from a number of well-known
+        multinational corporations, universities and SMEs to venture capital
+        firms, tech start-ups and many successful entrepreneurs, all of whom
+        benefit from our breadth of expertise and experience.
+      </p>
+      <p>
+        We are one of only a handful of boutique, mid-sized patent and trade
+        mark attorney firms in Australia and clients benefit from accessing the
+        expertise and systems of a large firm at mid-tier rates with
+        predictable, capped fees.
+      </p>
+      <p>
+        Uniquely, qualifying clients can access our{' '}
+        <a href="https://www.baxterip.com.au/capital-partners">capital</a> and
+        commercialisation partners to assist them with raising capital or in
+        taking steps involved in commercialising their intellectual property.
+      </p>
+      <h3>Most awarded mid-sized patent &amp; trade mark firm in Australia</h3>
+      <p>
+        We are one of the most awarded mid-sized firms in Australia,
+        particularly for our expertise in software patenting. In addition, many
+        of our attorneys have individually received awards too.
+      </p>
+      <h3>Outstanding and proven client service track record</h3>
+      <p>
+        Whilst we are appreciative of the awards we have received, what is much
+        more important to us is the experience clients have in working with us.
+        We survey our clients every six months and our latest Net Promoter Score
+        was an outstanding 77 in 2021. Net Promoter score is the normative
+        scoring method for client happiness used by large corporations and
+        scores range from -100 to +100.
+      </p>
+      <h3>Strong patents &amp; experienced trade mark counsel</h3>
+      <p>
+        By leveraging the industry specific expertise of{' '}
+        <a href="https://www.baxterip.com.au/patent-attorney">
+          our patent attorneys
+        </a>
+        , we can pursue the broadest possible patent coverage for your idea or
+        technology. Awarded for thinking outside the box, we excel in{' '}
+        <a href="https://www.baxterip.com.au/pct-patent">
+          international patenting
+        </a>{' '}
+        and mitigating future risk by careful and comprehensive counsel. Our{' '}
+        <a href="https://www.baxterip.com.au/provisional-patents">
+          patent applications
+        </a>{' '}
+        not only meet international standards but are prepared by patent
+        attorneys who are specialists in their fields.
+      </p>
+      <p>
+        From in-house experience managing hundreds of trade marks for large
+        international brands to addressing cease and desist letters from
+        international corporations, our trade mark attorneys can strengthen and
+        defend your brand. Our trade mark team is always discussing latest case
+        outcomes and developments in case law and practice on our internal
+        professional development platform, so we are poised to give your
+        valuable trade mark the best opportunity for success.
+      </p>
+      <h3>Advice reinforced by IP enforcement and litigation experience</h3>
+      <p>
+        We work closely with lawyers and barristers to defend and enforce client
+        IP rights, effectively providing end-to-end service for contentious
+        matters. We filter the learnings and knowledge from contentious cases
+        and patent litigation back up to our patent and trade mark attorneys, so
+        we can draft stronger, more defensible patent and trade mark
+        applications. If the ability to enforce your intellectual property is
+        part of your commercial risk mitigation strategy, you are in no better
+        hands than Baxter IP.
+      </p>
+    </Layout>
+  )
+}
