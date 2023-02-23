0bb10eb4a4fb40709d9cbc6e935b5dd4d58cb391
Gabriel Luis <gabriel@lilrobots.com>
2023-02-22 21:26:52 -0500

    Update [slug].js

--- pages/ip-news/[slug].js
+++ pages/ip-news/[slug].js
@@ -77,12 +77,12 @@ export async function getStaticProps({ params, preview = false }) {
       preview,
       data: {
         post,
-        author,
         morePosts: overlayDrafts(morePosts),
       },
     },
     // If webhooks isn't setup then attempt to re-generate in 1 minute intervals
-    revalidate: process.env.SANITY_REVALIDATE_SECRET ? undefined : 60,
+    // revalidate: process.env.SANITY_REVALIDATE_SECRET ? undefined : 60,
+    revalidate: process.env.SANITY_REVALIDATE_SECRET ? undefined : 1,
   }
 }
 
