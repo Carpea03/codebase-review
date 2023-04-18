9c9b1063cb9e421bd5e2cfbcd5f992f935fb659e
Gabriel Luis <gabriel@lilrobots.com>
2023-04-18 11:27:32 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/flowbite-react/**/*.js",
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
       fontFamily: {
-        manrope: ["Manrope"],
-        lora: ["Lora"],
+        manrope: ['Manrope'],
+        lora: ['Lora'],
       },
     },
     screens: {
-      sm: "640px",
+      sm: '640px',
       // => @media (min-width: 640px) { ... }
-      md: "834px",
+      md: '834px',
       // => @media (min-width: 834px) { ... }
-      lg: "1024px",
+      lg: '1024px',
       // => @media (min-width: 1024px) { ... }
-      xl: "1280px",
+      xl: '1280px',
       // => @media (min-width: 1280px) { ... }
-      "2xl": "1440px",
+      '2xl': '1440px',
       // => @media (min-width: 1440px) { ... }
     },
   },
-  plugins: [require("flowbite/plugin"), require("tailwind-scrollbar-hide")],
-};
+  plugins: [require('tailwind-scrollbar-hide')],
+}
