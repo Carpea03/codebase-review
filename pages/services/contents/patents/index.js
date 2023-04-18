255d3f10d2cd17854e8bdb8c5a0749da3ba7167f
Gabriel Luis <gabriel@lilrobots.com>
2023-04-18 14:19:39 -0500

    Update index.js

--- pages/services/contents/patents/index.js
+++ pages/services/contents/patents/index.js
@@ -13,11 +13,11 @@ export default function Patents({ menus }) {
       <div className="w-full flex flex-row items-center bg-[#F3F3FA] overflow-x-auto scrollbar-hide">
         {menus.map((menu, index) => (
           <div
+            key={index}
             className={`flex-1 flex-col items-center p-6 cursor-pointer ${
               selectedMenu === index ? 'border-b-4 border-[#8C79D9]' : ''
             }`}
-            onClick={() => setSelectedMenu(index)}
-            key={index}>
+            onClick={() => setSelectedMenu(index)}>
             <span className="font-manrope font-semibold text-sm sm:text-xl text-[#404266] whitespace-nowrap">
               {menu.title}
             </span>
