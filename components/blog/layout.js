cc86428d75d90839d9814d17057c000c54a1eb81
Gabriel Luis <gabriel@lilrobots.com>
2023-04-18 12:55:45 -0500

    Update layout.js

--- components/blog/layout.js
+++ components/blog/layout.js
@@ -1,10 +1,10 @@
 import Alert from './alert'
-import Contact from './to-delete/blocks/contact'
-import Footer from './templates/Footer'
-import Header from './templates/header'
+import Contact from '../to-delete/blocks/contact'
+import Footer from '../templates/Footer'
+import Header from '../templates/Header'
 import Meta from './meta'
-import Newsletter from './to-delete/blocks/newsletter'
-import RelatedArticle from './to-delete/blocks/related-article'
+import Newsletter from '../to-delete/blocks/newsletter'
+import RelatedArticle from '../to-delete/blocks/related-article'
 
 export default function Layout({ preview, children }) {
   return (
