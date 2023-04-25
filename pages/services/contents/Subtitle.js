0a5c01dfe1c9d187ad3562cab83bdb37810cfe91
Gabriel Luis <gabriel@lilrobots.com>
2023-04-25 12:53:14 -0500

    Add services template

---
+++ pages/services/contents/Subtitle.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Subtitle({ children }) {
+  return (
+    <span className="font-lora font-medium sm:text-[32px] text-xl md:text-2xl text-[#272940]">
+      {children}
+    </span>
+  );
+}
---
+++ pages/services/contents/patents/index.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+import PCTNationalPhaseAustralia from './PCTNationalPhaseAustralia'
+
+export default function Patents({ menus }) {
+  const [selectedMenu, setSelectedMenu] = useState(0)
+  return (
+    <>
+      <div className="w-full flex flex-row items-center bg-[#F3F3FA] overflow-x-auto scrollbar-hide">
+        {menus.map((menu, index) => (
+          <div
+            key={index}
+            className={`flex-1 flex-col items-center p-6 cursor-pointer ${
+              selectedMenu === index ? 'border-b-4 border-[#8C79D9]' : ''
+            }`}
+            onClick={() => setSelectedMenu(index)}>
+            <span className="font-manrope font-semibold text-sm sm:text-xl text-[#404266] whitespace-nowrap">
+              {menu.title}
+            </span>
+          </div>
+        ))}
+      </div>
+      {selectedMenu === 0 && <PCTNationalPhaseAustralia />}
+    </>
+  )
+}
