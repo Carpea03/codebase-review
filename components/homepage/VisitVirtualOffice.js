e72349f5dcfe926e7f2cc2c60b4bf5d4b1372ce1
ritobalaquiao14 <ritobalaquiao14@gmail.com>
2023-07-05 23:56:24 +0800

    Change into virtual office

--- components/homepage/VisitVirtualOffice.js
+++ components/homepage/VisitVirtualOffice.js
@@ -12,11 +12,11 @@ export const VisitVirtualOffice = () => {
         <div className="flex flex-col md:flex-row justify-center md:justify-between items-center w-full px-12 pt-[60px] sm:px-24 sm:pt-[134px] md:px-40 md:pt-[120px] pb-12 sm:pb-[107px] md:pb-[70px]">
           <div className="w-full md:w-1/2 flex flex-col items-center md:items-start gap-[14px] sm:gap-[10px]">
             <span className="uppercase font-manrope font-bold text-xs sm:text-2xl md:text-xl text-[#40320F]/50">
-              Meta office
+              Virtual office
             </span>
             <div className="flex flex-row items-center py-5 gap-[10px] px-9 sm:px-20 md:px-0">
               <span className="font-lora font-medium text-xl sm:text-5xl md:leading-[70px] text-[#272940] text-center md:text-left">
-                Meet Us in Baxter IP Meta Office
+                Meet Us in Baxter IP Virtual Office
               </span>
             </div>
           </div>
@@ -37,7 +37,7 @@ export const VisitVirtualOffice = () => {
               className="w-full flex flex-row justify-center items-center py-5 sm:py-10 gap-[10px] bg-[#333552]"
             >
               <span className="font-manrope font-semibold text-sm sm:text-xl text-white">
-                Visit meta office now
+                Visit virtual office now
               </span>
               <IoIosArrowForward
                 className="w-4 h-4 sm:w-6 sm:h-6"
