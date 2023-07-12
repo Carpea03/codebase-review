e0fb793c20a4feb9f29484e83dc4ea875a9ebe92
ritobalaquiao14 <ritobalaquiao14@gmail.com>
2023-07-12 13:04:52 +0800

    update blog file

--- components/to-delete/layout.js
+++ components/to-delete/layout.js
@@ -4,7 +4,7 @@ export default function Layout({ children }) {
   return (
     <>
       <Header active={'Blog'} />
-      <main className="format max-w-none mx-auto px-5 lg:px-20 pb-10">
+      <main className="format max-w-none mx-auto px-5 lg:px-20 pb-10 pt-10">
         {children}
       </main>
       <Footer />
