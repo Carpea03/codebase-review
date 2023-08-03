e2ee9c176931fb06768f39cb10175f0c6e773878
Rito Balaquiao <ritobalaquiao14@gmail.com>
2023-08-03 23:53:33 +0800

    Revised Visit Virtual Office

--- components/homepage/VisitVirtualOffice.js
+++ components/homepage/VisitVirtualOffice.js
@@ -1,6 +1,6 @@
 import { Container } from '../templates/Container'
 import React from 'react'
-import { IoIosArrowForward } from 'react-icons/io'
+import { IoMdArrowRoundForward } from 'react-icons/io'
 import Link from 'next/link'
 import { metaOffice } from '../../utils/const/links'
 import { InnerContainer } from '../templates/InnerContainer'
@@ -14,37 +14,61 @@ export const VisitVirtualOffice = () => {
         <InnerContainer>
           <div className="flex flex-col md:flex-row justify-center md:justify-between items-center w-full px-12 pt-[60px] sm:px-24 sm:pt-[134px] md:px-40 md:pt-[120px] pb-12 sm:pb-[107px] md:pb-[70px]">
             <div className="w-full md:w-1/2 flex flex-col items-center md:items-start gap-[14px] sm:gap-[10px]">
+              <span className="font-lora font-medium text-xl sm:text-2xl md:leading-[30px] text-[#272940] text-center md:text-left opacity-50">
+                {"VIRTUAL OFFICE"}
+              </span>
               <div className="flex flex-row items-center py-5 gap-[10px] px-9 sm:px-20 md:px-0">
-                <span className="font-lora font-medium text-xl sm:text-5xl md:leading-[70px] text-[#272940] text-center md:text-left">
-                  {"Meet an IP Attorney in Baxter IP's Virtual Office Now"}
+                <span
+                  style={{ lineHeight: 1.5 }}
+                  className="font-lora font-medium text-xl sm:text-5xl md:leading-[10px] text-[#272940] text-center md:text-left"
+                >
+                  {"Meet us in the Baxter IP Virtual Office"}
+                </span>
+              </div>
+              <div className="flex flex-row items-center py-5 gap-[10px] px-9 md:px-0">
+                <span>
+                  We specialise aross various market segments, Your bussiness provides
+                  products or services that make society better. 
+                  Our patent & trademark attorneys bring you profession-leading experience
+                  in sector-specific IP to protect your valuable tech or brand.
                 </span>
               </div>
             </div>
-            <div className="md:w-1/2 md:flex flex-col items-end md:pl-[108px] text-black opacity-50">
-              <span>
-                With a single click you will find yourself at the reception area
-                of our virtual office. No special equipment is required and you
-                can leave your video off if you wish.
-                <br />
-                <br />
-                One of our friendly receptionists will greet you, take you to a
-                meeting room, then introduce you to a team member who can
-                discuss your query with you.
-              </span>
+            <div className="md:w-1/2 md:flex flex-col items-end md:pl-[20px] md:pt-[40px]">
+              <div className="relative w-full pt-[20px] sm:pt-0 md:pt-10">
+              <div className="absolute top-[110px] sm:top-[160px] w-full pl-[100px] text-center">
+                <Link href={metaOffice}>
+                  <div
+                    className="w-52 h-11 sm:w-[434px] sm:h-24 md:w-80 md:h-[60px] bg-[#816BD9] rounded-md flex flex-row justify-center items-center gap-6 sm:gap-x-14 md:gap-4 cursor-pointer hover:opacity-80"
+                    style={{
+                      boxShadow:
+                        '0px 56px 114px rgba(150, 151, 169, 0.17), 0px 12.5083px 25.4634px rgba(150, 151, 169, 0.101338), 0px 3.72406px 7.58112px rgba(150, 151, 169, 0.0686618), 0px 1.54966px 3.15467px rgba(0, 0, 0, 0.0477948)',
+                    }}
+                  >
+                    <IoMdArrowRoundForward
+                      style={{transform: 'rotate(-45deg)' }} 
+                        className="w-4 h-4 sm:w-6 sm:h-6"
+                        color="white"
+                    />
+                    <span className=" font-manrope font-semibold text-[10px] sm:text-xl text-white">
+                      Visit meta office
+                    </span>
+                  </div>
+                </Link>
+              </div>
+                {/** bottom */}
+                <Image
+                  alt=""
+                  src="/meta-office new.png"
+                  className="w-full"
+                  width={1440}
+                  height={955}
+                />
+              </div>
             </div>
           </div>
         </InnerContainer>
-        {/** bottom */}
-        <div className="w-full">
-          <Image
-            alt=""
-            src="/Meta office.svg"
-            className="w-full"
-            width={1440}
-            height={955}
-          />
-        </div>
-        <div className="w-full bg-[#333552]">
+        {/* <div className="w-full bg-[#333552]">
           <InnerContainer>
             <Link href={metaOffice}>
               <button
@@ -61,7 +85,7 @@ export const VisitVirtualOffice = () => {
               </button>
             </Link>
           </InnerContainer>
-        </div>
+        </div> */}
       </div>
     </Container>
   )
