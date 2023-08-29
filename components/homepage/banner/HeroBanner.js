0cba022854f7bfe8fed7c99e188d0204699fd151
ritobalaquiao14 <ritobalaquiao14@gmail.com>
2023-08-29 23:31:10 +0800

    Changing hero banner

--- components/homepage/banner/HeroBanner.js
+++ components/homepage/banner/HeroBanner.js
@@ -1,13 +1,12 @@
 import Button from '../../buttons/Button'
 import { Container } from '../../templates/Container'
 import { InnerContainer } from '../../templates/InnerContainer'
-import React from 'react'
+import React, { useState } from 'react'
 import { HiPhone } from 'react-icons/hi'
 import { IoArrowForwardOutline } from 'react-icons/io5'
 import { BsDoorOpenFill } from 'react-icons/bs'
 import Image from 'next/image'
 import Link from 'next/link'
-import { metaOffice } from '../../../utils/const/links'
 
 const details = [
   {
@@ -23,86 +22,110 @@ const details = [
 ]
 
 const buttonText = [
-  { title: 'Talk to an IP attorney' },
-  { title: 'Visit our Virtual Office' },
+  { title: 'Talk to an IP attorney now' },
+  { title: 'Visit our virtual office now' },
 ]
 
 export default function HeroBanner({ landing, data }) {
+  const [changeImage, setChangeImage] = useState(false)
   return (
-    <Container className="z-4 0 md:px-20 xl:px-40 bg-hero-banner">
-      <div className="container max-w-screen-2xl mx-auto h-full">
-        <div className="flex flex-col md:flex-row items-center md:items-stretch justify-center gap-16 md:gap-0 py-10 sm:py-[60px] md:py-16 xl:py-32">
-          {/** contents */}
-          <div className="md:w-[50%] xl:w-full flex flex-col items-center md:items-start gap-[70px] order-last md:order-first">
-            <div className="w-full flex flex-col items-start justify-center gap-6">
-              <span className="w-full font-lora font-semibold text-2xl sm:text-5xl lg:text-6xl xl:text-[59px] xl:leading-[120%] text-[#272940] text-center md:text-left px-10 md:px-0">
-                {landing ? data?.header[0]?.title : 'Innovate Boldly.'}
-                <br />
-                {landing ? '' : 'Protect Strategically.'}
-              </span>
-              <p
-                style={{ whiteSpace: 'pre-line' }}
-                className="font-manrope font-semibold text-sm sm:text-2xl text-[#272940]/50 text-center md:text-left px-14 sm:px-40 md:px-0"
-              >
-                {landing ? data?.header[0]?.body : details[0].description}
-              </p>
-            </div>
-            <div className="flex flex-col md:flex-row md:justify-center md:items-center gap-6 md:gap-6">
-              <Link href="/contact-us">
-                <div className="flex flex-col items-center">
-                  <div
-                    className="w-60 h-20 sm:w-[434px] sm:h-24 md:w-80 md:h-[88px] bg-[#816BD9] rounded-md flex flex-row justify-center items-center gap-6 sm:gap-x-14 md:gap-4 cursor-pointer hover:opacity-80"
-                    style={{
-                      boxShadow:
-                        '0px 56px 114px rgba(150, 151, 169, 0.17), 0px 12.5083px 25.4634px rgba(150, 151, 169, 0.101338), 0px 3.72406px 7.58112px rgba(150, 151, 169, 0.0686618), 0px 1.54966px 3.15467px rgba(0, 0, 0, 0.0477948)',
-                    }}
-                  >
-                   <Image src="/telephone.svg" alt="" width={48} height={48} />
-                    <span className="font-manrope font-semibold text-[14px] sm:text-xl text-white">
-                      {buttonText[0].title}
-                    </span>
-                  </div>
+    <Container className="z-4 bg-hero-banner px-10 xl:px-0">
+      <InnerContainer>
+        <div className="xl:h-[620px] xl:pt-20">
+          <div className="container 2xl:flex 2xl:justify-center h-full pt-12 xl:pt-0 md:py-20 xl:px-10 ">
+            <div className="flex flex-col xl:flex-row">
+              <div className="order-2 xl:order-1 xl:w-[700px] pt-[60px]">
+                <span className="w-full flex justify-center text-center  xl:justify-center 2xl:justify-start 2xl:text-left font-lora font-semibold text-5xl lg:text-6xl xl:text-[56px] xl:leading-[1-0%] text-[#272940] px-10 md:px-0 ">
+                  {landing ? data?.header[0]?.title : 'Innovate Boldly.'}
+                  <br />
+                  {landing ? '' : 'Protect Strategically.'}
+                </span>
+                <p
+                  style={{ whiteSpace: 'pre-line' }}
+                  className="pb-20 md:pb-10 pt-10 font-manrope font-semibold text-2xl text-[#272940]/50 text-center 2xl:text-left xl:w-[700px]"
+                >
+                  {' '}
+                  {landing ? data?.header[0]?.body : details[0].description}
+                </p>
+                <div className="flex flex-col gap-5 md:flex-row 2xl:pt-14 pb-10 sm:pb-10 md:pb-0 xl:pb-0 2xl:pb-10 justify-center 2xl:justify-start">
+                  <Link href="/contact-us">
+                    <div className="flex flex-col items-center px-2">
+                      <div
+                        className="w-60 h-[87px] w-[330px] sm:h-24 sm:w-[330px] md:w-[309px] md:h-[88px] bg-[#816BD9] rounded-md flex flex-row justify-center items-center gap-6 sm:gap-x-14 md:gap-4 cursor-pointer hover:opacity-80"
+                        style={{
+                          boxShadow:
+                            '0px 56px 114px rgba(150, 151, 169, 0.17), 0px 12.5083px 25.4634px rgba(150, 151, 169, 0.101338), 0px 3.72406px 7.58112px rgba(150, 151, 169, 0.0686618), 0px 1.54966px 3.15467px rgba(0, 0, 0, 0.0477948)',
+                        }}
+                      >
+                        <Image
+                          src="/telephone.svg"
+                          alt=""
+                          width={48}
+                          height={48}
+                        />
+                        <span className="font-manrope font-semibold text-[14px] sm:text-xl text-white w-[180px]">
+                          {buttonText[0].title}
+                        </span>
+                      </div>
+                    </div>
+                  </Link>
+                  <Link href={'/'}>
+                    <div
+                      className="flex flex-col items-center"
+                      onMouseEnter={() => setChangeImage(true)}
+                      onMouseLeave={() => setChangeImage(false)}
+                    >
+                      <div
+                        className={`w-60 h-[87px] w-[330px] sm:h-24 sm:w-[330px] md:w-[330px] xl:w-[330px] 2xl:w-[330px] sm:h-[88px] md:h-[88px] ${
+                          changeImage
+                            ? 'pointerArrow rounded-tl-md rounded-bl-md'
+                            : 'bg-[#816BD9] rounded-md'
+                        }  flex flex-row justify-center items-center gap-6 sm:gap-x-14 md:gap-4 cursor-pointer 
+                        ${changeImage ? 'pointerArrow' : ''} `}
+                        style={
+                          changeImage
+                            ? { backgroundImage: `url("/arrrow-bg.svg")` }
+                            : {
+                                boxShadow:
+                                  '0px 56px 114px rgba(150, 151, 169, 0.17), 0px 12.5083px 25.4634px rgba(150, 151, 169, 0.101338), 0px 3.72406px 7.58112px rgba(150, 151, 169, 0.0686618), 0px 1.54966px 3.15467px rgba(0, 0, 0, 0.0477948)',
+                              }
+                        }
+                      >
+                        <Image src="/door.svg" alt="" width={48} height={48} />
+                        <span className="font-manrope font-semibold text-[14px] sm:text-xl text-white w-[200px]">
+                          {buttonText[1].title}
+                        </span>
+                      </div>
+                    </div>
+                  </Link>
                 </div>
-              </Link>
-              <Link href={metaOffice}>
-                <div className="z-40 group flex flex-col items-center hover:bg-[#9A85D2] rounded">
-                  <div
-                    className="w-60 h-20 sm:w-[434px] sm:h-24 md:w-80 md:h-[88px] group-hover:bg-[#9A85D2] bg-[#816BD9] rounded-md flex flex-row justify-center items-center gap-6 sm:gap-x-14 md:gap-4 cursor-pointer"
-                    style={{
-                      boxShadow:
-                        '0px 56px 114px rgba(150, 151, 169, 0.17), 0px 12.5083px 25.4634px rgba(150, 151, 169, 0.101338), 0px 3.72406px 7.58112px rgba(150, 151, 169, 0.0686618), 0px 1.54966px 3.15467px rgba(0, 0, 0, 0.0477948)',
-                    }}
-                  >
-                    <Image src="/door.svg" alt="" width={48} height={48} />
-                    <span className=" font-manrope font-semibold text-[14px] sm:text-xl text-white">
-                      {buttonText[1].title}
-                    </span>
-                  </div>
-                  <div className="z-40 flex justify-center ">
-                    <span className=" absolute z-50 -mt-2 scale-0 transition-none bg-[#9A85D2] p-2 group-hover:scale-100 rounded">
-                      <Image
-                        src="/virtual-office.png"
-                        alt=""
-                        width={304}
-                        height={300}
-                      />
-                    </span>
-                  </div>
-                </div>
-              </Link>
+              </div>
+              <div className="order-1 xl:order-2 flex justify-center xl:justify-end">
+                <Image
+                  src={
+                    changeImage
+                      ? '/meta-office.png'
+                      : '/professionalProfiles/IP-people.jpg'
+                  }
+                  alt={'banner'}
+                  width={584}
+                  height={438}
+                  style={
+                    changeImage
+                      ? {
+                          borderWidth: 10,
+                          borderStyle: 'solid',
+                          borderColor: '#806AD9',
+                          borderRadius: 5,
+                        }
+                      : {}
+                  }
+                />
+              </div>
             </div>
           </div>
-          <Image
-            src="/professionalProfiles/IP-people.jpg"
-            className={`${
-              landing ? 'pt-10' : ''
-            } md:h-[300px] md:w-full xl:h-full xl:w-full object-cover md:pl-5`}
-            alt=""
-            width={442}
-            height={332}
-          />
         </div>
-      </div>
+      </InnerContainer>
     </Container>
   )
 }
