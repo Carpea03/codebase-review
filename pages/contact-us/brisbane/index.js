bf192a1178d90037143420dc980e64f41b3f9604
Rito Balaquiao <ritobalaquiao14@gmail.com>
2023-08-31 22:40:36 +0800

    Separted page in contact-us per office

--- pages/contact-us/brisbane/index.js
+++ pages/contact-us/brisbane/index.js
@@ -1,36 +1,55 @@
-import { Container } from '../../components/templates/Container'
-import Footer from '../../components/templates/Footer'
-import Header from '../../components/templates/Header'
-import React, { useState, useRef } from 'react'
-import { FiChevronUp } from 'react-icons/fi'
-import Contact from './offices/Contact'
-import { contactDetails } from '../../utils/const/contacts'
+import { Container } from '../../../components/templates/Container'
+import Footer from '../../../components/templates/Footer'
+import Header from '../../../components/templates/Header'
+import React, { useState, useEffect } from 'react'
+import Contact from '../offices/Contact'
+import { contactDetails } from '../../../utils/const/contacts'
 import { useRouter } from 'next/router'
-import { InnerContainer } from '../../components/templates/InnerContainer'
-import Head from 'next/head'
+import { InnerContainer } from '../../../components/templates/InnerContainer'
 
-const hears = [
-  { id: 1, name: 'Select one' },
-  { id: 2, name: 'LinkedIn' },
-]
-
-export default function ContactUs() {
-  const router = useRouter()
+export default function ContactUs({ office }) {
   const [callUs, setCallUs] = useState(true)
+  const router = useRouter()
   const [selectedId, setSelectedId] = useState(0)
-  const [selectedPlace, setSelectedPlace] = useState('+61 2 9264 671')
+  const [selectedPlace, setSelectedPlace] = useState()
+  const [place, setPlace] = useState([])
+
+  useEffect(() => {
+
+      let office = null;
+      let newArray = []
+
+      var url = window.location;
+
+      var area = url.toString().substring(url.toString().lastIndexOf('/') + 1).toLowerCase()
+
+      contactDetails.sort().map((key) => {
+        newArray.push({ ...key })
+      })
+
+      setPlace(newArray)
+
+      office = contactDetails.filter(
+        (item) => item.name.toLowerCase() === area
+      )[0]
+
+      setSelectedPlace(office?.number)
+      setSelectedId(office?.id)
+    
+  }, [office])
 
   const onSelected = (id) => {
     const details = contactDetails[id]
-    setSelectedPlace(details.number)
-    setSelectedId(id)
-    // router.push(`/contact-us/${details.name.toLocaleLowerCase()}`)
+    // setSelectedPlace(details.number)
+    // setSelectedId(id)
+    router.push(`/contact-us/${details.name.toLocaleLowerCase()}`)
   }
 
-  const renderOptions = (item, id) => (
+  const renderOptions = (id, item, key) => (
     <option
-      key={`contact-${id}`}
+      key={`contact-${key}`}
       value={id}
+      selected={id == selectedId ? 'selected' : ''}
       className="flex font-manrope font-semibold text-[#404266] sm:text-xl md:text-xl whitespace-nowrap"
     >
       {item}
@@ -39,26 +58,22 @@ export default function ContactUs() {
 
   return (
     <>
-      <Head>
-        <title>Contact Us</title>
-        <meta name="description" content="" />
-      </Head>
       <Header active={'Contact'} />
       <Container className="bg-[#FFFDF7]">
-        <div className="w-full bg-contactus-unsplash !bg-cover">
-          <div
-            style={{
-              background: 'rgba(64, 66, 102, 0.7)',
-            }}
-          >
+        <InnerContainer>
+          <div className="w-full bg-contactus-unsplash !bg-cover">
             <div
-              className="flex flex-col items-start gap-4"
               style={{
-                background:
-                  'linear-gradient(180deg, rgba(255, 206, 79, 0) 0%, rgba(255, 206, 79, 0.4) 100%)',
+                background: 'rgba(64, 66, 102, 0.7)',
               }}
             >
-              <InnerContainer>
+              <div
+                className="flex flex-col items-start gap-4"
+                style={{
+                  background:
+                    'linear-gradient(180deg, rgba(255, 206, 79, 0) 0%, rgba(255, 206, 79, 0.4) 100%)',
+                }}
+              >
                 <div className="w-full flex flex-col items-center md:items-start px-4 sm:px-[76px] md:pl-40 py-28 sm:py-[409px] md:py-48">
                   <div className="w-full md:w-[558px] h-44 sm:h-80 md:h-[185px] flex flex-col items-center gap-6 sm:gap-[60px] md:gap-6">
                     <span className="font-lora font-medium text-3xl sm:text-5xl sm:leading-[138%] md:text-[40px] md:leading-[51px] text-white text-center md:text-left">
@@ -68,9 +83,10 @@ export default function ContactUs() {
                       <select
                         onChange={(e) => onSelected(e.target.value)}
                         className="select-contact flex flex-row items-center px-5 py-4 sm:py-9 sm:px-5 md:px-6 md:py-4 gap-[10px] bg-[#FFCE4F] cursor-pointer h-full rounded-l-md flex font-manrope font-semibold text-[#404266] text-xl whitespace-nowrap"
+                        // value={'Call us Brisbane Office'}
                       >
-                        {contactDetails.map((item, index) =>
-                          renderOptions(item?.place, index)
+                        {place?.map((item, index) =>
+                          renderOptions(item?.id, item?.place, index)
                         )}
                       </select>
                       <div className="flex flex-row items-center px-5 py-4 sm:py-9 sm:px-6 md:px-6 md:py-4 gap-[10px] bg-black/30 cursor-pointer h-full rounded-r-md">
@@ -81,11 +97,9 @@ export default function ContactUs() {
                     </div>
                   </div>
                 </div>
-              </InnerContainer>
+              </div>
             </div>
           </div>
-        </div>
-        <InnerContainer>
           <Contact contactDetails={contactDetails[selectedId]} />
         </InnerContainer>
       </Container>
--- pages/contact-us/melbourne/index.js
+++ pages/contact-us/melbourne/index.js
@@ -1,54 +1,11 @@
-import { Container } from '../../components/templates/Container'
-import Footer from '../../components/templates/Footer'
-import Header from '../../components/templates/Header'
+import { Container } from '../../../components/templates/Container'
+import Footer from '../../../components/templates/Footer'
+import Header from '../../../components/templates/Header'
 import React, { useState, useEffect } from 'react'
-import Contact from './offices/Contact'
-import { contactDetails } from '../../utils/const/contacts'
+import Contact from '../offices/Contact'
+import { contactDetails } from '../../../utils/const/contacts'
 import { useRouter } from 'next/router'
-import { InnerContainer } from '../../components/templates/InnerContainer'
-import Head from 'next/head'
-
-const hears = [
-  { id: 1, name: 'Select one' },
-  { id: 2, name: 'LinkedIn' },
-]
-
-const offices = [
-  {
-    id: 'sydney',
-    title: 'Sydney Patent & Trade Mark Office',
-    details: 'Call us Sydney Office',
-    office: 'Sydney Office',
-  },
-  {
-    id: 'melbourne',
-    title: 'Melbourne Patent & Trade Mark Office',
-    details: 'Call us Melbourne Office',
-    office: 'Melbourne Office',
-  },
-]
-
-const ipOffices = ['sydney', 'melbourne', 'brisbane']
-
-export const getStaticPaths = async () => {
-  const paths = ipOffices.map((item) => {
-    return {
-      params: { id: item.toString() },
-    }
-  })
-
-  return {
-    paths,
-    fallback: false,
-  }
-}
-
-export const getStaticProps = async (context) => {
-  const id = context.params.id
-  return {
-    props: { office: id },
-  }
-}
+import { InnerContainer } from '../../../components/templates/InnerContainer'
 
 export default function ContactUs({ office }) {
   const [callUs, setCallUs] = useState(true)
@@ -58,49 +15,41 @@ export default function ContactUs({ office }) {
   const [place, setPlace] = useState([])
 
   useEffect(() => {
-    if (office) {
-      const item = contactDetails.filter(
-        (item) => item.name.toLowerCase() === office.toLowerCase()
-      )[0]
 
+      let office = null;
       let newArray = []
 
-      if (item.id === 0) {
-        setSelectedPlace(item.number)
-        contactDetails.sort().map((key) => {
-          newArray.push({ ...key })
-        })
-        setPlace(newArray)
-        return
-      }
+      var url = window.location;
 
-      if (item.id === 2) {
-        setSelectedPlace(item.number)
-        contactDetails.reverse().map((key) => {
-          newArray.push({ ...key })
-        })
-        setPlace(newArray)
-        return
-      }
-      setSelectedPlace(contactDetails[0].number)
-      setPlace(contactDetails)
-    }
-    setSelectedPlace(contactDetails[0].number)
-    setPlace(contactDetails)
+      var area = url.toString().substring(url.toString().lastIndexOf('/') + 1).toLowerCase()
+
+      contactDetails.sort().map((key) => {
+        newArray.push({ ...key })
+      })
+
+      setPlace(newArray)
+
+      office = contactDetails.filter(
+        (item) => item.name.toLowerCase() === area
+      )[0]
+
+      setSelectedPlace(office?.number)
+      setSelectedId(office?.id)
+    
   }, [office])
 
   const onSelected = (id) => {
     const details = contactDetails[id]
-    setSelectedPlace(details.number)
-    setSelectedId(id)
+    // setSelectedPlace(details.number)
+    // setSelectedId(id)
     router.push(`/contact-us/${details.name.toLocaleLowerCase()}`)
   }
 
-  const renderOptions = (item, id, selected) => (
+  const renderOptions = (id, item, key) => (
     <option
-      key={`contact-${id}`}
+      key={`contact-${key}`}
       value={id}
-      selected={selected}
+      selected={id == selectedId ? 'selected' : ''}
       className="flex font-manrope font-semibold text-[#404266] sm:text-xl md:text-xl whitespace-nowrap"
     >
       {item}
@@ -109,26 +58,22 @@ export default function ContactUs({ office }) {
 
   return (
     <>
-      <Head>
-        <title>Contact Us</title>
-        <meta name="description" content="" />
-      </Head>
       <Header active={'Contact'} />
       <Container className="bg-[#FFFDF7]">
-        <div className="w-full bg-contactus-unsplash !bg-cover">
-          <div
-            style={{
-              background: 'rgba(64, 66, 102, 0.7)',
-            }}
-          >
+        <InnerContainer>
+          <div className="w-full bg-contactus-unsplash !bg-cover">
             <div
-              className="flex flex-col items-start gap-4"
               style={{
-                background:
-                  'linear-gradient(180deg, rgba(255, 206, 79, 0) 0%, rgba(255, 206, 79, 0.4) 100%)',
+                background: 'rgba(64, 66, 102, 0.7)',
               }}
             >
-              <InnerContainer>
+              <div
+                className="flex flex-col items-start gap-4"
+                style={{
+                  background:
+                    'linear-gradient(180deg, rgba(255, 206, 79, 0) 0%, rgba(255, 206, 79, 0.4) 100%)',
+                }}
+              >
                 <div className="w-full flex flex-col items-center md:items-start px-4 sm:px-[76px] md:pl-40 py-28 sm:py-[409px] md:py-48">
                   <div className="w-full md:w-[558px] h-44 sm:h-80 md:h-[185px] flex flex-col items-center gap-6 sm:gap-[60px] md:gap-6">
                     <span className="font-lora font-medium text-3xl sm:text-5xl sm:leading-[138%] md:text-[40px] md:leading-[51px] text-white text-center md:text-left">
@@ -138,10 +83,10 @@ export default function ContactUs({ office }) {
                       <select
                         onChange={(e) => onSelected(e.target.value)}
                         className="select-contact flex flex-row items-center px-5 py-4 sm:py-9 sm:px-5 md:px-6 md:py-4 gap-[10px] bg-[#FFCE4F] cursor-pointer h-full rounded-l-md flex font-manrope font-semibold text-[#404266] text-xl whitespace-nowrap"
-                        value={'Call us Brisbane Office'}
+                        // value={'Call us Brisbane Office'}
                       >
                         {place?.map((item, index) =>
-                          renderOptions(item?.place, index, item?.selected)
+                          renderOptions(item?.id, item?.place, index)
                         )}
                       </select>
                       <div className="flex flex-row items-center px-5 py-4 sm:py-9 sm:px-6 md:px-6 md:py-4 gap-[10px] bg-black/30 cursor-pointer h-full rounded-r-md">
@@ -152,11 +97,9 @@ export default function ContactUs({ office }) {
                     </div>
                   </div>
                 </div>
-              </InnerContainer>
+              </div>
             </div>
           </div>
-        </div>
-        <InnerContainer>
           <Contact contactDetails={contactDetails[selectedId]} />
         </InnerContainer>
       </Container>
