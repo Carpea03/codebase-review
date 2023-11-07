0fe2e44bbc9690ecd2a58b9719b15528b976573a
Gabriel Luis <gabriel@lilrobots.com>
2023-11-07 18:26:34 -0500

    Add redirects on missing pages with backlinks

--- next.config.js
+++ next.config.js
@@ -11,4 +11,49 @@ module.exports = {
       'maps.googleapis.com',
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/patent-design-australia',
+        destination: '/australian-registered-design',
+        permanent: true,
+      },
+      {
+        source: '/global/',
+        destination: '/global',
+        permanent: true,
+      },
+      {
+        source: '/melbourne/binh-rey',
+        destination: '/melbourne',
+        permanent: true,
+      },
+      {
+        source: '/patent-attorney-profiles',
+        destination: '/patent-attorney',
+        permanent: true,
+      },
+      {
+        source: '/fastcircle',
+        destination: '/ip-strategy',
+        permanent: true,
+      },
+      {
+        source: '/patent-specialisations',
+        destination: '/specialisations',
+        permanent: true,
+      },
+      {
+        source: '/after-patent-pending',
+        destination: '/services',
+        permanent: true,
+      },
+      {
+        source: '/patent-applications',
+        destination: '/provisional-patents',
+        permanent: true,
+      },
+      // Add more redirects here as required
+    ]
+  },
 }
